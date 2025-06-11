e012e1e7cd3751698a9e08c230dce1112417c1e1
Fran Azuara <franazuara@gmail.com>
2025-06-11 13:19:02 +0200

    event interface

---
+++ backend/src/Interfaces/event.interface.ts
@@ -0,0 +1,11 @@
+export interface Event {
+  id: string;
+  title: String;
+  description: String;
+  style: String;
+  start: String;
+  end: String;
+  location: String;
+  date: String;
+  teacher: String;
+}
