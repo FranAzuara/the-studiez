505147f15316d5c40fb2f28c0c4c86a67ae6aa29
Fran Azuara <franazuara@gmail.com>
2025-06-13 11:59:11 +0200

    protected routes with jwt

--- backend/src/routes/event.ts
+++ backend/src/routes/event.ts
@@ -6,14 +6,16 @@ import {
   updateEvent,
   deleteEvent,
 } from "../controllers/event";
+import { checkJwt } from "../middleware/session";
 // Este archivo define las rutas para manejar eventos en la aplicación.
 // Importa las funciones del controlador de eventos que manejan la lógica de negocio.
 // Utiliza el enrutador de Express para definir las rutas y asociarlas con las funciones del controlador.
+// Las rutas incluyen una autenticación JWT para las operaciones de creación, actualización y eliminación de eventos.
 
 const router = Router();
 router.get("/:id", getEvent);
 router.get("/", getEvents);
-router.post("/", createEvent);
-router.put("/:id", updateEvent);
-router.delete("/:id", deleteEvent);
+router.post("/", checkJwt, createEvent);
+router.put("/:id", checkJwt, updateEvent);
+router.delete("/:id", checkJwt, deleteEvent);
 export { router };
