56f0d1f8acc9a927900a0bf463b1a3958ca32b54
Fran Azuara <franazuara@gmail.com>
2025-06-14 18:46:23 +0200

    new navbar component

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-black p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">THE STUDIEZ</div>
+        <div className="space-x-4">
+          <a href="/">Inicio</a>
+          <a href="/about">Nosotros</a>
+          <a href="/event">Eventos</a>
+          <a href="/contact">Contacto</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
