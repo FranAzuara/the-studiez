48832d674090acc11bc5b11aad2702ee909b81e2
Fran Azuara <franazuara@gmail.com>
2025-06-24 19:38:12 +0200

    href add <a>

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,13 +1,24 @@
 const Navbar = () => {
   return (
-    <nav className="bg-black p-4 fixed top-0 right-0 left-0">
+    <nav className="bg-black p-4 fixed top-0 right-0 left-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">THE STUDIEZ</div>
+        <div className="flex items-center space-x-4">
+          <img src="src/TheStudiezL.png" className="w-10 h-auto" />
+          <div className="text-white text-lg font-bold">THE STUDIEZ</div>
+        </div>
         <div className="space-x-4">
-          <a href="/">Inicio</a>
-          <a href="/about">Nosotros</a>
-          <a href="/event">Eventos</a>
-          <a href="/contact">Contacto</a>
+          <a className="a-navbar" href="#inicio">
+            Inicio
+          </a>
+          <a className="a-navbar" href="#nosotros">
+            Nosotros
+          </a>
+          <a className="a-navbar" href="#eventos">
+            Eventos
+          </a>
+          <a className="a-navbar" href="#contacto">
+            Contacto
+          </a>
         </div>
       </div>
     </nav>
