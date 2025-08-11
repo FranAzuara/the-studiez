03badccb53db89405de12aa17f5852d92e4db60a
Fran Azuara <franazuara@gmail.com>
2025-08-11 10:43:08 +0200

    calendar routes (backend)

---
+++ backend/src/routes/calendar.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { getAvailability, updateAvailability } from "../controllers/calendar";
+import { checkJwt } from "../middleware/session";
+
+const router = Router();
+
+router.get("/", getAvailability);
+router.put("/", checkJwt, updateAvailability);
+
+export { router };
