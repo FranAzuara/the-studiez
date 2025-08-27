2a6507a6a3b5ff8a7db8359c286259134b0ec666
Fran Azuara <franazuara@gmail.com>
2025-08-27 12:25:37 +0200

    delete galery compenent import, add "prueba" component  import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,10 @@ import EventCarousel from "./components/eventCarousel";
 import EditEventModal from "./components/editEventModal";
 import Footer from "./components/footer";
 import { type EventType } from "./types/events";
-import GaleryCarousel from "./components/galerycarousel";
 import WeekCalendar from "./components/weekCalendar";
 import LoginModal from "./components/loginModal";
 import { motion } from "framer-motion";
+import Prueba from "./components/prueba";
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -96,7 +96,7 @@ function App() {
   // Para la sección contacto
   const subject = encodeURIComponent("Reserva de espacio THE STUDIEZ");
   const body = encodeURIComponent(
-    "Hola nuenos dias, me gustaría reservar su refugio el [DIA] de [HORARIO]..."
+    "Hola buenos días, me gustaría reservar su refugio el [DIA] de [HORA COMIENZO] a [HORA FINAL]..."
   );
   const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=the.studiez.bcn@gmail.com&su=${subject}&body=${body}`;
 
@@ -122,129 +122,7 @@ function App() {
           </p>
         </section>
 
-        {/* Sobre Nosotros */}
-        <motion.section
-          id="nosotros"
-          className="bg-white rounded-lg shadow-lg pb-4 scroll-mt-20 mx-4 md:mx-auto bg-[url('/degradadoverde.jpg')] bg-cover bg-center"
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-        >
-          <motion.h2
-            id="nosotros"
-            className="text-3xl md:text-6xl p-4 text-center font-bold"
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            viewport={{ once: true, amount: 0.6 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
-          >
-            <h2>Sobre Nosotros</h2>
-          </motion.h2>
-          <div className="grid grid-cols-2">
-            {/* Columna 1 */}
-            <div className="flex flex-col items-start justify-center lg:items-center ms-4">
-              <h3 className="text-2xl md:text-4xl pb-1">Nuestro Refugio</h3>
-              <p className="mt-0">Un espacio para moverte, crear y brillar.</p>
-            </div>
-
-            {/* Columna 2 vacía */}
-            <div></div>
-          </div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-4">
-            {/* Nuestro Espacio */}
-            <motion.div
-              initial={{ opacity: 0, x: -100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ duration: 0.6, ease: "easeOut", delay: 0.1 }}
-            >
-              <div className="text-justify text-base md:text-lg leading-relaxed">
-                <p className="mt-4">
-                  Nuestra sala, con sus <strong>100 m²</strong> de amplitud, es
-                  mucho más que cuatro paredes: es un escenario en blanco listo
-                  para transformarse en lo que sueñes.
-                </p>
-                <p className="mt-3">
-                  Las paredes blancas reflejan la luz y la energía; el{" "}
-                  <strong>suelo de parquet</strong> acoge cada paso con calidez;
-                  la <strong>iluminación LED</strong> ajustable dibuja la
-                  atmósfera perfecta, ya sea íntima o vibrante.
-                </p>
-                <p className="mt-3">
-                  La <strong>música</strong> cobra vida gracias a un equipo
-                  profesional con cuatro <strong>altavoces</strong> y un{" "}
-                  <strong>subwoofer</strong> que llenan la sala de{" "}
-                  <strong>sonido envolvente.</strong> A tu disposición también
-                  tienes <strong>focos de grabación</strong> que realzan cada
-                  movimiento y una <strong>máquina de humo</strong> que añade
-                  ese toque de magia a tus sesiones.
-                </p>
-                <p className="mt-3">
-                  Para que disfrutes sin importar la temporada, la sala está
-                  equipada con <strong>aire acondicionado</strong>, ideal para
-                  mantener la frescura en los días de calor intenso.
-                </p>
-                <p className="mt-3">
-                  Además, contamos con dos <strong>baños</strong> y una práctica{" "}
-                  <strong>zona de almacenaje</strong>, perfecta para dejar
-                  chaquetas, mochilas o lo que necesites.
-                </p>
-                <p className="mt-3">
-                  Bailar, grabar, ensayar, fotografiar… aquí{" "}
-                  <strong>no hay límites</strong>. Este es tu lienzo, y{" "}
-                  <strong>tú decides</strong> la obra que vas a crear.
-                </p>
-              </div>
-            </motion.div>
-
-            {/* GaleryCarousel */}
-            <motion.div
-              className="flex items-center justify-center"
-              initial={{ opacity: 0, x: 100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ duration: 0.6, ease: "easeOut", delay: 0.2 }}
-            >
-              <GaleryCarousel />
-            </motion.div>
-          </div>
-
-          {/* Nuestros Valores */}
-          <motion.div
-            className="p-4"
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true, amount: 0.4 }}
-            transition={{ duration: 0.6, ease: "easeOut", delay: 0.3 }}
-          >
-            <p className="text-4xl text-center">Qué nos define</p>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-              <div className="bg-white rounded-lg drop-shadow-lg p-4">
-                <strong>Calidad profesional</strong>
-                <p>
-                  Un espacio cuidado al detalle, equipado con materiales y
-                  tecnología que garantizan el mejor resultado para tus clases,
-                  ensayos o producciones.
-                </p>
-              </div>
-              <div className="bg-white rounded-lg drop-shadow-lg p-4">
-                <strong>Ambiente acogedor</strong>
-                <p>
-                  Un lugar pensado para que cualquiera que cruce la puerta se
-                  sienta cómodo, inspirado y con ganas de crear.
-                </p>
-              </div>
-              <div className="bg-white rounded-lg drop-shadow-lg p-4">
-                <strong>Impulso creativo</strong>
-                <p>
-                  Facilitamos que cada proyecto crezca, aportando recursos y
-                  flexibilidad para adaptarnos a tus necesidades.
-                </p>
-              </div>
-            </div>
-          </motion.div>
-        </motion.section>
+        <Prueba />
 
         {/* Eventos */}
         <section id="eventos" className="p-4 scroll-mt-20">
@@ -260,7 +138,7 @@ function App() {
           id="disponibilidad"
           className="p-4 mx-4 md:mx-auto scroll-mt-20"
         >
-          <h2 className="text-4xl p-4 text-center font-bold">
+          <h2 className="text-4xl text-white bg-black/70 w-fit mx-auto p-4 text-center rounded-lg font-bold">
             Disponibilidad Semanal
           </h2>
           <motion.div
@@ -276,7 +154,7 @@ function App() {
         {/* Contacto */}
         <section
           id="contacto"
-          className="bg-white rounded-lg shadow-lg p-4 mx-4 md:mx-auto scroll-mt-20"
+          className="bg-white/90 rounded-lg shadow-lg p-4 mx-4 md:mx-auto scroll-mt-20"
         >
           <h3 className="text-4xl p-4 text-center font-bold">
             Reserva tu momento en la pista
@@ -293,9 +171,14 @@ function App() {
               </p>
               <p>Elige la fecha, reserva tu hora y empieza a hacerla tuya. </p>
               <a href={gmailUrl} target="_blank" rel="noopener noreferrer">
-                <button className="bg-green-500 text-white px-4 py-2 rounded-lg mt-2">
+                <motion.button
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  transition={{ type: "spring", stiffness: 300 }}
+                  className="bg-green-500 text-white px-4 py-2 rounded-lg mt-2 shadow-md"
+                >
                   Contáctanos
-                </button>
+                </motion.button>
               </a>
             </div>
           </div>
