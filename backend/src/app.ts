37dfb969c38ecc710a4619e0dd1e9384b13271c7
Fran Azuara <franazuara@gmail.com>
2025-09-02 12:47:14 +0200

    cambios par deploy vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import { router } from "./routes";
 import db from "./config/mongo";
+import serverless from "serverless-http";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,7 +11,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 db().then(() => console.log("Conectado a la base de datos"));
-app.listen(PORT, () => {
-  console.log(`Listo por el port ${PORT}`);
-});
+export const handler = serverless(app);
 // Este archivo es el punto de entrada de la aplicación Express.
