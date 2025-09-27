a65873fa636a0a227465792a59ac4e39c0cfce49
Fran Azuara <franazuara@gmail.com>
2025-09-27 14:23:01 +0200

    fix: update CORS origin to use HTTPS for production environment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? ["http://the-studiez-backend.vercel.app"]
+        ? ["https://the-studiez-backend.vercel.app"]
         : ["http://localhost:3001"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
